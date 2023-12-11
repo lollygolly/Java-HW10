ae4462a3c06b2a5fe2a7a5f2a6fb2c32e4a0c037
LollyGolly <valentina_96@inbox.ru>
2023-12-11 22:50:40 +0500

    Code fixed

--- src/main/java/ru/netology/services/Radio.java
+++ src/main/java/ru/netology/services/Radio.java
@@ -1,11 +1,18 @@
 package ru.netology.services;
 
 public class Radio {
-    public int stationNumber; // Вводим класс номера радиостанции
+    private int stationNumber; // СОЗДАЕМ ПОЛЕ, В КОТОРОМ БУДУТ ХРАНИТСЯ НОМЕРА РАДИОСТАНЦИЙ
+    private int soundVolume; // СОЗДАЕМ ПОЛЕ, В КОТОРОМ БУДУТ ХРАНИТСЯ ЗНАЧЕНИЯ ГРОМКОСТИ ЗВУКА
+
 
     public int getStationNumber() { // Метод получения текущего номера радиостанции
         return stationNumber;
     }
+
+    public int getSoundVolume() { // Метод получения текущей громкости звука
+        return soundVolume;
+    }
+
 
     public void setStationNumber(int newStationNumber) { //Метод изменения номера радиостанции с ограничением от 0 до 9
         if (newStationNumber < 0) {
@@ -15,32 +22,6 @@ public class Radio {
             return;
         }
         stationNumber = newStationNumber;
-    }
-
-    public void next() { // Метод переключения номера радиостанции на следующий
-        if (stationNumber < 9) {
-            stationNumber = stationNumber + 1;
-        }
-        if(stationNumber >= 9) {
-            stationNumber = 0;
-        }
-    }
-
-
-    public void prev() { // Метод переключения номера радиостанции на следующий
-        if (stationNumber > 0) {
-            stationNumber = stationNumber - 1;
-        }
-        if (stationNumber <= 0) {
-            stationNumber = 9;
-        }
-    }
-
-
-    public int soundVolume; // Вводим класс громкости звука радиоприемника
-
-    public int getGetSoundVolume() { // Метод получения текущей громкости звука
-        return soundVolume;
     }
 
     public void setSoundVolume(int newSoundVolume) { //Метод изменения текущей громкости с ограничением от 0 до 100
@@ -53,16 +34,34 @@ public class Radio {
         soundVolume = newSoundVolume;
     }
 
+
+    public void next() { // Метод переключения номера радиостанции на следующий
+        if (stationNumber != 9) {
+            stationNumber++;
+        } else {
+            stationNumber = 0;
+        }
+    }
+
+    public void prev() { // Метод переключения номера радиостанции на предыдущий
+        if (stationNumber != 0) {
+            stationNumber--;
+        } else {
+            stationNumber = 9;
+        }
+    }
+
+
     public void increaseSoundVolumeByOne() { //Метод увеличения громкости на один
         if (soundVolume < 100) {
             soundVolume = soundVolume + 1;
         }
     }
 
-        public void decreaseSoundVolumeByOne() { //Метод уменьшения громкости на один
-            if (soundVolume > 0) {
-                soundVolume = soundVolume - 1;
-            }
+    public void decreaseSoundVolumeByOne() { //Метод уменьшения громкости на один
+        if (soundVolume > 0) {
+            soundVolume = soundVolume - 1;
         }
     }
+}
 
--- src/test/java/ru/netology/services/RadioTest.java
+++ src/test/java/ru/netology/services/RadioTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 public class RadioTest {
 
     @Test
-    public void shouldGetStationNumber() {
+    public void shouldSetStationNumber() { // 1. ТЕСТИРУЕМ НОМЕР СТАНЦИИ
         Radio rad = new Radio();
-        rad.stationNumber = 5;
+        rad.setStationNumber(5);
 
         int expected = 5;
         int actual = rad.getStationNumber();
@@ -16,32 +16,59 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldSetStationNumberWithinBorders() {
+    public void shouldNotSetStationNumberBelowZero() { // 2. ОЧЕНЬ МАЛЕНЬКОЕ ЗНАЧЕНИЕ
         Radio rad = new Radio();
-        rad.setStationNumber(3);
+        rad.setStationNumber(-5);
 
-        int expected = 3;
-        int actual = rad.getStationNumber();
-        Assertions.assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void shouldNotSetStationNumberBelowZero() {
-        Radio rad = new Radio();
-        rad.stationNumber = 2;
-        rad.setStationNumber(-2);
-
-        int expected = 2;
+        int expected = 0;
         int actual = rad.getStationNumber();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotSetStationNumberAboveZero() {
+    public void shouldNotSetStationNumberAboveZero() { // 3. ОЧЕНЬ БОЛЬШОЕ ЗНАЧЕНИЕ
         Radio rad = new Radio();
-        rad.stationNumber = 8;
-        rad.setStationNumber(11);
+        rad.setStationNumber(15);
+
+        int expected = 0;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetStationNumberBeforeFirstBorder() { // 4. ЗНАЧЕНИЕ ДО ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(-1);
+
+        int expected = 0;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationNumberOnFirstBorder() { // 5. ЗНАЧЕНИЕ НА ПЕРВОЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setStationNumber(0);
+
+        int expected = 0;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationNumberAfterFirstBorder() { // 6. ЗНАЧЕНИЕ ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(1);
+
+        int expected = 1;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetStationNumberBeforeLastBorder() { // 7. ЗНАЧЕНИЕ ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(8);
 
         int expected = 8;
         int actual = rad.getStationNumber();
@@ -49,21 +76,30 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldNextStationNumberWithinBorders() {
+    public void shouldSetStationNumberOnLastBorder() { // 8. ЗНАЧЕНИЕ НА ПОСЛЕДНЕЙ ГРАНИЦЕ
         Radio rad = new Radio();
-        rad.setStationNumber(7);
+        rad.setStationNumber(9);
+
+        int expected = 9;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetStationNumberAfterLastBorder() { // 9. ЗНАЧЕНИЕ ПОСЛЕ ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(10);
+
+        int expected = 0;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextStationNumberWithinBorders() { // 10. ПЕРЕКЛЮЧЕНИЕ НА СЛЕДУЮЩУЮ СТАНЦИЮ В ГРАНИЦАХ
+        Radio rad = new Radio();
+        rad.setStationNumber(5);
         rad.next();
-
-        int expected = 8;
-        int actual = rad.getStationNumber();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldPrevStationNumberWithinBorders() {
-        Radio rad = new Radio();
-        rad.setStationNumber(7);
-        rad.prev();
 
         int expected = 6;
         int actual = rad.getStationNumber();
@@ -71,7 +107,40 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldReturnFirstStationNumber() {
+    public void shouldNextStationOnFirstBorder() { // 12. ПЕРЕКЛЮЧЕНИЕ НА СЛЕДУЮЩУЮ СТАНЦИЮ НА ПЕРВОЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setStationNumber(0);
+        rad.next();
+
+        int expected = 1;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextStationAfterFirstBorder() { // 13. ПЕРЕКЛЮЧЕНИЕ НА СЛЕДУЮЩУЮ СТАНЦИЮ ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(1);
+        rad.next();
+
+        int expected = 2;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextStationBeforeLastBorder() { // 14. ПЕРЕКЛЮЧЕНИЕ НА СЛЕДУЮЩУЮ СТАНЦИЮ ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(8);
+        rad.next();
+
+        int expected = 9;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnFirstStationOnLastBorder() { // 15. ПЕРЕКЛЮЧЕНИЕ НА СЛЕДУЮЩУЮ СТАНЦИЮ НА ПОСЛЕДНЕЙ ГРАНИЦЕ
         Radio rad = new Radio();
         rad.setStationNumber(9);
         rad.next();
@@ -81,8 +150,20 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+
     @Test
-    public void shouldReturnLastStationNumber() {
+    public void shouldPrevStationNumberWithinBorders() { // 17. ПЕРЕКЛЮЧЕНИЕ НА ПРЕДЫДУЩУЮ СТАНЦИЮ В ГРАНИЦАХ
+        Radio rad = new Radio();
+        rad.setStationNumber(5);
+        rad.prev();
+
+        int expected = 4;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNReturnLastStationNumberOnFirstBorder() { // 19. ПЕРЕКЛЮЧЕНИЕ НА ПРЕДЫДУЩУЮ СТАНЦИЮ НА ПЕРВОЙ ГРАНИЦЕ
         Radio rad = new Radio();
         rad.setStationNumber(0);
         rad.prev();
@@ -93,89 +174,237 @@ public class RadioTest {
     }
 
     @Test
-    public void shouldGetSoundVolume() {
+    public void shouldNPrevStationNumberAfterFirstBorder() { // 20. ПЕРЕКЛЮЧЕНИЕ НА ПРЕДЫДУЩУЮ СТАНЦИЮ ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
         Radio rad = new Radio();
-        rad.soundVolume = 50;
+        rad.setStationNumber(1);
+        rad.prev();
+
+        int expected = 0;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevStationNumberBeforeLastBorder() { // 21. ПЕРЕКЛЮЧЕНИЕ НА ПРЕДЫДУЩУЮ СТАНЦИЮ ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setStationNumber(8);
+        rad.prev();
+
+        int expected = 7;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevStationNumberOnLastBorder() { // 22. ПЕРЕКЛЮЧЕНИЕ НА ПРЕДЫДУЩУЮ СТАНЦИЮ НА ПОСЛЕДНЕЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setStationNumber(9);
+        rad.prev();
+
+        int expected = 8;
+        int actual = rad.getStationNumber();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetSoundVolume() { // 24. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА В ГРАНИЦАХ
+        Radio rad = new Radio();
+        rad.setSoundVolume(50);
 
         int expected = 50;
-        int actual = rad.getGetSoundVolume();
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldSetSoundVolumeWithinBorders() {
+    public void shouldNotSetSoundVolumeBelowZero() { // 25. ОЧЕНЬ МАЛЕНЬКОЕ ЗНАЧЕНИЕ ГРОМКОСТИ ЗВУКА
         Radio rad = new Radio();
-        rad.soundVolume = 50;
-        rad.setSoundVolume(70);
-
-        int expected = 70;
-        int actual = rad.getGetSoundVolume();
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldNotSetSoundVolumeBelowZero() {
-        Radio rad = new Radio();
-        rad.soundVolume = 50;
         rad.setSoundVolume(-50);
 
-        int expected = 50;
-        int actual = rad.getGetSoundVolume();
+        int expected = 0;
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotSetSoundVolumeAboveAHundred() {
+    public void shouldNotSetSoundVolumeAboveAHundred() { // 26. ОЧЕНЬ БОЛЬШОЕ ЗНАЧЕНИЕ ГРОМКОСТИ ЗВУКА
         Radio rad = new Radio();
-        rad.soundVolume = 50;
         rad.setSoundVolume(120);
 
-        int expected = 50;
-        int actual = rad.getGetSoundVolume();
+        int expected = 0;
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldIncreaseSoundVolumeWithinBorders() {
+    public void shouldNotSetSoundVolumeBeforeFirstBorder() { // 27. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА ДО ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(-1);
+
+        int expected = 0;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetSoundVolumeOnFirstBorder() { // 28. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА НА ПЕРВОЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setSoundVolume(0);
+
+        int expected = 0;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetSoundVolumeOnAfterBorder() { // 29. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(1);
+
+        int expected = 1;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetSoundVolumeBeforeLastBorder() { // 30. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(99);
+
+        int expected = 99;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetSoundVolumeOnLastBorder() { // 31. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА НА ПОСЛЕДНЕЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setSoundVolume(100);
+
+        int expected = 100;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSetSoundVolumeAfterLastBorder() { // 32. ИЗМЕНЯЕМ ГРОМКОСТЬ ЗВУКА ПОСЛЕ ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(101);
+
+        int expected = 0;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseSoundVolumeWithinBorders() { // 33. УВЕЛИЧИВАЕМ ГРОМКОСТЬ В ГРАНИЦАХ
         Radio rad = new Radio();
         rad.setSoundVolume(50);
         rad.increaseSoundVolumeByOne();
 
         int expected = 51;
-        int actual = rad.getGetSoundVolume();
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldDecreaseSoundVolumeWithinBorders() {
+    public void shouldIncreaseSoundVolumeOnFirstBorder() { // 35. УВЕЛИЧИВАЕМ ГРОМКОСТЬ НА ПЕРВОЙ ГРАНИЦЕ
         Radio rad = new Radio();
-        rad.setSoundVolume(50);
-        rad.decreaseSoundVolumeByOne();
+        rad.setSoundVolume(0);
+        rad.increaseSoundVolumeByOne();
 
-        int expected = 49;
-        int actual = rad.getGetSoundVolume();
+        int expected = 1;
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotIncreaseSoundVolumeAboveAHundred() {
+    public void shouldIncreaseSoundVolumeAfterFirstBorder() { // 36. УВЕЛИЧИВАЕМ ГРОМКОСТЬ ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(1);
+        rad.increaseSoundVolumeByOne();
+
+        int expected = 2;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseSoundVolumeBeforeLastBorder() { // 37. УВЕЛИЧИВАЕМ ГРОМКОСТЬ ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(99);
+        rad.increaseSoundVolumeByOne();
+
+        int expected = 100;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotIncreaseSoundVolumeOnLastBorder() { // 38. УВЕЛИЧИВАЕМ ГРОМКОСТЬ НА ПОСЛЕДНЕЙ ГРАНИЦЕ
         Radio rad = new Radio();
         rad.setSoundVolume(100);
         rad.increaseSoundVolumeByOne();
 
         int expected = 100;
-        int actual = rad.getGetSoundVolume();
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldNotDecreaseSoundVolumeBelowZero() {
+    public void shouldDecreaseSoundVolumeWithinBorders() { // 40. УМЕНЬШАЕМ ГРОМКОСТЬ В ГРАНИЦАХ
+        Radio rad = new Radio();
+        rad.setSoundVolume(50);
+        rad.decreaseSoundVolumeByOne();
+
+        int expected = 49;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotDecreaseSoundVolumeOnFirstBorder() { // 42. УМЕНЬШАЕМ ГРОМКОСТЬ НА ПЕРВОЙ ГРАНИЦЕ
         Radio rad = new Radio();
         rad.setSoundVolume(0);
         rad.decreaseSoundVolumeByOne();
 
         int expected = 0;
-        int actual = rad.getGetSoundVolume();
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseSoundVolumeAfterFirstBorder() { // 43. УМЕНЬШАЕМ ГРОМКОСТЬ ПОСЛЕ ПЕРВОЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(1);
+        rad.decreaseSoundVolumeByOne();
+
+        int expected = 0;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseSoundVolumeBeforeLastBorder() { // 44. УМЕНЬШАЕМ ГРОМКОСТЬ ДО ПОСЛЕДНЕЙ ГРАНИЦЫ
+        Radio rad = new Radio();
+        rad.setSoundVolume(99);
+        rad.decreaseSoundVolumeByOne();
+
+        int expected = 98;
+        int actual = rad.getSoundVolume();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseSoundVolumeOnLastBorder() { // 45. УМЕНЬШАЕМ ГРОМКОСТЬ НА ПОСЛЕДНЕЙ ГРАНИЦЕ
+        Radio rad = new Radio();
+        rad.setSoundVolume(100);
+        rad.decreaseSoundVolumeByOne();
+
+        int expected = 99;
+        int actual = rad.getSoundVolume();
         Assertions.assertEquals(expected, actual);
     }
 }
+
+
